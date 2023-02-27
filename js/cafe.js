d57f0f2263a6c9b77455f58f0bd0b7feed9309d1
JefferySeo <125331815+JefferySeo@users.noreply.github.com>
2023-02-27 16:11:52 +0900

    0227수업

--- js/cafe.js
+++ js/cafe.js
@@ -1,34 +1,60 @@
 $(function(){
     //검색
-    $('.search-select').click(function(e){
+   $('.search-select').click(function(e){
       e.preventDefault();  //a 태그의 기능을 중지 시킴
       //search-sbox의 display 값을 받아다가 변수 db에 저장한다.
-       const dp =  $('.search-sbox').css("display"); 
-       //db 값이 none 일때와 block일때를 구분한다.
-       if(dp == "none"){
-          $('.selectbox').find('.fa-solid').removeClass('fa-angle-down').addClass('fa-angle-up');//none이라면 아래 화살표를 지우고 위에 화살표를 넣는다.
-          //search-sbox를 display block으로 바꾼다.        
-          $('.search-sbox').show();
-       }else{
-          //none과 반대의 처리를 한다.
-          $('.selectbox').find('.fa-solid').removeClass('fa-angle-up').addClass('fa-angle-down');
-          $('.search-sbox').hide();
-       }        
-    });
-  
-     $('.search-sbox>a').click(function(e){
-         e.preventDefault();  //a 태그의 기능을 중지 시킴
-         const txt = $(this).text();
-         $('.search-select').text(txt);
-         $('.search-sbox').hide();
+      const dp =  $('.search-sbox').css("display"); 
+      //db 값이 none 일때와 block일때를 구분한다.
+      if(dp == "none"){
+         $('.selectbox').find('.fa-solid').removeClass('fa-angle-down').addClass('fa-angle-up');//none이라면 아래 화살표를 지우고 위에 화살표를 넣는다.
+         //search-sbox를 display block으로 바꾼다.        
+         $('.search-sbox').show();
+      }else{
+         //none과 반대의 처리를 한다.
          $('.selectbox').find('.fa-solid').removeClass('fa-angle-up').addClass('fa-angle-down');
-         $('#selectbox').val(txt);
-         $('#searchtext').focus();
-     })
-
-     $('.hero li').mouseenter(function(){
-        $('.hero li').removeClass('active');
-        $(this).addClass('active');
-     })
+         $('.search-sbox').hide();
+      }        
+   });
   
-  });  //jquery
+   $('.search-sbox>a').click(function(e){
+      e.preventDefault();  //a 태그의 기능을 중지 시킴
+      const txt = $(this).text();
+      $('.search-select').text(txt);
+      $('.search-sbox').hide();
+      $('.selectbox').find('.fa-solid').removeClass('fa-angle-up').addClass('fa-angle-down');
+      $('#selectbox').val(txt);
+      $('#searchtext').focus();
+   })
+
+   $('.hero li').mouseenter(function(){
+      $('.hero li').removeClass('active');
+      $(this).addClass('active');
+   })
+
+
+   // slide show
+
+   setInterval(mySlide, 10000);
+   $('.next').click(mySlide);
+});  //jquery
+
+function mySlide(){
+   const eq0 = $('.hero .new:eq(0)'); // 최초로 보이는 new
+   const eq1 = $('.hero .new:eq(1)'); // 그 뒤에 숨어있는 new
+   // animate를 통해서 두 번째 new에 zindex를 추가하여
+   // 제일 앞에 보이게 하고 투명처리 한 후 점차 진하게 보이게 한다.
+   eq1.addClass('zindex').css('opacity',0).animate({
+      'opacity':1
+   },500,function(){
+      // animate 작업이 끝나면 이전에 보였던 new의 zindex를 지우고
+      // 가장 나중으로 바꿔준다. 
+      eq1.find('li').eq(randomList()).addClass('active');
+      eq0.removeClass('zindex');
+      eq0.find('li').removeClass('active');
+      $('.hero').append(eq0);
+      
+   });
+}
+function randomList(){
+   return Math.floor(Math.random()*4);
+}
